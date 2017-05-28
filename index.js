00a7cf3c9d3b8211a5ca265927c0b4db5c1810e8
drklrd <srvbhattarai@gmail.com>
2017-05-28 21:55:25 +0545

    implement very basic word cloud

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-
+var jsdom = require("jsdom/lib/old-api.js");
 var alphabets = 'abcdefghijklmnopqrstuvwxyz';
 
 fs
@@ -8,6 +8,7 @@ fs
 		return (file.indexOf(".") !== 0) && (file !== "index.js") && (file.split(".")[1] === "js");
 	})
 	.forEach(function(file) {
+		
 		var fileContent = fs.readFileSync(file, {
 			encoding: 'utf-8'
 		});
@@ -17,6 +18,7 @@ fs
 		var contentClone = strippedContent;
 		var ctstring = "";
 		var words = {};
+
 		for (var char = 0; char < strippedContent.length; char++) {
 			if (alphabets.indexOf(strippedContent[char]) === -1) {
 				if (words[ctstring] || words[ctstring] === 0) {
@@ -30,7 +32,23 @@ fs
 			}
 		}
 
-		console.log(words);
+		jsdom.env(
+			"<html><body></body></html>", ['http://d3js.org/d3.v3.min.js'],
+			function(err, window) {
+				var svg = window.d3.select("body")
+					.append("svg")
+					.attr("width", 1000).attr("height", 1000);
 
+				for (var word in words) {
+					svg.append("text")
+						.attr("x", Math.floor(Math.random() * 1000))
+						.attr("y", Math.floor(Math.random() * 1000))
+						.text(word)
+				}
+
+				fs.writeFileSync('out.html', "<html><body>" + window.d3.select("body").html() + "</body></html>"); // or this
+
+			}
+		);
 
 	})
