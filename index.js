c97d36dc69e5d1d687a1c7f903ddbd2bd3ead556
drklrd <srvbhattarai@gmail.com>
2017-05-29 22:12:37 +0545

    read files using recursive-readdir

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 var fs = require('fs');
 var jsdom = require("jsdom/lib/old-api.js");
 var alphabets = 'abcdefghijklmnopqrstuvwxyz';
+var recursive = require("recursive-readdir");
 
 var svgDimensions = {
 	width: 1500,
 	height: 800
 };
 
-function sortObj(obj){
+function sortObj(obj) {
 
 	var sortedArray = [];
 	var sortedobj = {};
 	for (var word in words) {
-	    sortedArray.push([word, words[word]]);
+		sortedArray.push([word, words[word]]);
 	}
 
 	sortedArray.sort(function(a, b) {
-	    return a[1] - b[1];
+		return a[1] - b[1];
 	});
 
-	sortedArray.forEach(function(sorted){
+	sortedArray.forEach(function(sorted) {
 		sortedobj[sorted[0]] = sorted[1];
 	});
 
@@ -33,15 +34,20 @@ var words = {};
 var totalWords = 0;
 var ctstring = "";
 
-fs
-	.readdirSync(__dirname + '/tests')
-	.filter(function(file) {
-		return (file.indexOf(".") !== 0) && (file !== "index.js") && (file.split(".")[1] === "js");
-	})
-	.forEach(function(file) {
 
-		console.log('Found ' + file);
-		var fileContent = fs.readFileSync(__dirname + '/tests/' + file, {
+
+recursive("./", ['node_modules', '.git'], function(err, files) {
+
+	var filesToRead = files.filter(function(file) {
+		return (file.indexOf(".") !== 0) && (file !== "index.js") && (file.split(".")[1] === "js");
+	});
+
+	console.log(`Found ${filesToRead.length} files ....`)
+
+	filesToRead.forEach(function(file) {
+
+		console.log('=====> ' + file);
+		var fileContent = fs.readFileSync(__dirname + '/' + file, {
 			encoding: 'utf-8'
 		});
 
@@ -62,78 +68,76 @@ fs
 
 	});
 
-for (var word in words) {
-	var allAlphabets = true;
-	if (word && word.length) {
-		for (var char = 0; char < word.length; char++) {
-			if (alphabets.indexOf(word[char]) === -1) {
-				allAlphabets = false;
-				break;
+	for (var word in words) {
+		var allAlphabets = true;
+		if (word && word.length) {
+			for (var char = 0; char < word.length; char++) {
+				if (alphabets.indexOf(word[char]) === -1) {
+					allAlphabets = false;
+					break;
+				}
 			}
-		}
-	} else {
-		allAlphabets = false;
-	}
-
-	if (!allAlphabets) {
-		delete words[word];
-	}
-};
-
-for (var word in words) {
-	totalWords = totalWords + words[word];
-};
-
-
-words = sortObj(words);
-
-console.log(words);
-
-var startingHex = '255';
-var colorChangeRate = Math.floor(Number(startingHex)/(Object.keys(words).length));
-
-jsdom.env(
-	"<html><body></body></html>", ['http://d3js.org/d3.v3.min.js'],
-	function(err, window) {
-
-		var svg = window.d3.select("body")
-			.append("svg")
-			.style("position","absolute")
-			.style("left","20%")
-			.attr("width", "100%")
-			.attr("height", "100%")
-			.append("g")
-			.attr("transform", "translate(250,250)");
-
-		var colorStep = 0;
-		for (var word in words) {
-			var textSize = Math.floor((words[word] * 100) / totalWords).toString();
-			svg.append("text")
-				.style("font-size", `${textSize*10+10}px`)
-				.style('fill',`rgb(${startingHex-colorStep},${startingHex-colorStep},${startingHex-colorStep})`)
-				.attr("x", (Math.random() < 0.5 ? -1 : 1) * Math.floor(Math.random() * (svgDimensions.width)/3))
-				.attr("y", (Math.random() < 0.5 ? -1 : 1) * Math.floor(Math.random() * (svgDimensions.height)/3)	)
-				.attr("text-anchor", "begin")
-				.attr("transform", `translate(300,150) rotate(${textSize})`)
-				.text(word)
-
-			if(Object.keys(words).length % colorStep !== colorChangeRate) {
-				colorStep  = colorStep + colorChangeRate;
-			}
-			
-
+		} else {
+			allAlphabets = false;
 		}
 
-		fs.writeFileSync('out.html', window.d3.select("body").html() + "<script> \
-				window.d3.select('body')\
-					.call(window.d3.behavior.zoom()\
-					.on('zoom',function(){  \
-						var text = `scale(${window.d3.event.scale})` ;  \
-						window.d3.select('body').style('transform',text)  \
-					})).append('g') \
-		</script>");
+		if (!allAlphabets) {
+			delete words[word];
+		}
+	};
 
-		console.log('Generated out.html');
+	for (var word in words) {
+		totalWords = totalWords + words[word];
+	};
 
-	}
-);
+	words = sortObj(words);
+	var startingHex = '255';
+	var colorChangeRate = Math.floor(Number(startingHex) / (Object.keys(words).length));
+
+	jsdom.env(
+		"<html><body></body></html>", ['http://d3js.org/d3.v3.min.js'],
+		function(err, window) {
+
+			var svg = window.d3.select("body")
+				.append("svg")
+				.attr("width", "100%")
+				.attr("height", "100%")
+				.append("g")
+				.attr("transform", "translate(250,250)");
+
+			var colorStep = 0;
+			for (var word in words) {
+				var textSize = Math.floor((words[word] * 100) / totalWords).toString();
+				svg.append("text")
+					.style("font-size", `${textSize*10+10}px`)
+					.style('fill', `rgb(${startingHex-colorStep},${startingHex-colorStep},${startingHex-colorStep})`)
+					.attr("x", (Math.random() < 0.5 ? -1 : 1) * Math.floor(Math.random() * (svgDimensions.width) / 3))
+					.attr("y", (Math.random() < 0.5 ? -1 : 1) * Math.floor(Math.random() * (svgDimensions.height) / 3))
+					.attr("text-anchor", "begin")
+					.attr("transform", `translate(300,150) rotate(${textSize})`)
+					.text(word)
+
+				colorStep = colorStep + colorChangeRate;
+
+
+			}
+
+			fs.writeFileSync('out.html', window.d3.select("body").html() + "<script> \
+  				window.d3.select('body')\
+  					.call(window.d3.behavior.zoom()\
+  					.on('zoom',function(){  \
+  						var text = `scale(${window.d3.event.scale})` ;  \
+  						window.d3.select('body').style('transform',text)  \
+  					})).append('g') \
+  		</script>");
+
+			console.log('Generated out.html');
+			console.log('DONE !!!!!!!!!!!!!');
+			process.exit();
+
+		}
+	);
+
+
+
+});
